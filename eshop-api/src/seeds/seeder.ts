dce573ae9aa669a421f77d71f1de674844059d1e
bluesky0724 <xing.liao724@gmail.com>
2023-04-21 10:00:56 +0900

    fix: update env variable

--- eshop-api/src/seeds/seeder.ts
+++ eshop-api/src/seeds/seeder.ts
@@ -6,15 +6,12 @@ import { data } from './seed-data';
 @Injectable()
 export class Seeder {
   constructor() {
-    const uri = 'mongodb://mongo:27017/nest';
+    const uri = process.env.MONGO_URI;
     (async () => {
       mongoose.connect(uri);
       // Seed the database
       await mongoose.connection.dropDatabase();
       await mongoose.connection.collection('products').insertMany(data);
-
-      // Disconnect from the MongoDB memory server
-      // await mongoose.disconnect();
     })();
   }
 }
